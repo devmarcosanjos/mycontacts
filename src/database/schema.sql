c881707848277f4d8f34754794bb32f5510dfdc8
marcos <dev.marcosanjos@gmail.com>
2024-12-23 20:04:20 -0300

    Add PostgreSQL support with database configuration and initial setup

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,17 +1,17 @@
 CREATE DATABASE mycontacts;
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE IF NOT EXISTS categories(
-  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
-  name VARCHAR NOT NULL
-);
+  id VARCHAR NOT NULL,
+  name VARCHAR NOT NULL,
+  PRIMARY KEY (id)
+  )
 
 CREATE TABLE IF NOT EXISTS contacts(
-  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  id VARCHAR NOT NULL,  -
   name VARCHAR NOT NULL,
   email VARCHAR UNIQUE,
   phone VARCHAR,
-  category_id UUID,
-  FOREIGN KEY(category_id) REFERENCES categories(id)
+  category_id NUMBER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (category_id) REFERENCES categories(id)
 );
